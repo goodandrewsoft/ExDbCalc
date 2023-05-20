d47d695479d3ad09dae965b060d22e57fdd65db5
andreyforpsn <134127667+andreyforpsn@users.noreply.github.com>
2023-05-20 10:15:00 +0300

    Create Test.java

---
+++ src/main/java/org/kinocat/tst/Test.java
@@ -0,0 +1,20 @@
+package org.kinocat;
+
+
+public class Test {
+
+
+    public void calculate() {
+      System.out.println("calculateWithBrackets2222");
+    }
+
+
+    public void calculateWithBrackets() {
+      System.out.println("calculateWithBrackets");
+    }
+
+
+    public void calcNumbers() {
+      System.out.println("sdsdsd);
+    }
+}
